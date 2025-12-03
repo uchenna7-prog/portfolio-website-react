12e490bd350b65aa435960e747f18fad53acfa1c
uchenna7-prog <uchenduuchenna7@gmail.com>
2025-12-03 20:36:11 +0100

    worked on mobile menu
    worked on mobile menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,31 +1,73 @@
 import styles from "./Header.module.css";
-import { useState } from "react";
+import { use, useState } from "react";
 import { Link } from "react-router-dom";
+import { useRef } from "react";
+
 
 function Header() {
-    const [theme,toogleTheme] = useState("light-mode")
+    const [theme,toggleTheme] = useState("light-mode")
+    const [menuOpen,setMenuOpen] = useState(false)
+    const mobileMenuRef = useRef(null);
+    const mobileMenuBtnRef = useRef(null);
+    const themeButtonRef = useRef(null);
+    const mobileMenuBtns = useRef([]);
+
+    const openMenu = ()=>{
+      if(!menuOpen){
+        mobileMenuRef.current.classList.add(styles.activeMobileMenu)
+        mobileMenuBtnRef.current.textContent = "close"
+        setMenuOpen(true)
+      }
+      else{
+        mobileMenuRef.current.classList.remove(styles.activeMobileMenu)
+        mobileMenuBtnRef.current.textContent = "menu"
+        setMenuOpen(false)
+      }
+    }
     const changeTheme = ()=>{
-      document.documentElement.getAttribute("data-theme") === "light-mode" ? document.documentElement.setAttribute("data-theme","dark-mode"):document.documentElement.setAttribute("data-theme","light-mode")
-      toogleTheme(document.documentElement.getAttribute("data-theme"))
+      let dataTheme = document.documentElement.getAttribute("data-theme")
+      if(dataTheme ==="light-mode"){
+        document.documentElement.setAttribute("data-theme","dark-mode")
+        themeButtonRef.current.textContent = "light_mode"
+      }
+      else{
+        document.documentElement.setAttribute("data-theme","light-mode")
+        themeButtonRef.current.textContent = "dark_mode"
+      }
+      toggleTheme(document.documentElement.getAttribute("data-theme"))
     }
   return (
-    <header className={styles.headerSection}>
-      <div className={styles.headerContainer}>
-        <span className={`${styles.logo} material-icons`}>home</span>
-        <nav className={styles.navLinksContainer}>
-          <a href="#about" className={styles.navLink}>About</a>
-          <a href="#services" className={styles.navLink}>Services</a>
-          <a href="#skills" className={styles.navLink}>Skills</a>
-          <a href="#projects" className={styles.navLink}>Projects</a>
-          <a href="#experience" className={styles.navLink}>Experience</a>
-          <a href="#contact" className={styles.navLink}>Contact</a>
-    
-          <span className={`${styles.toogleThemeBtn} material-icons`} onClick={()=>changeTheme()}>dark_mode</span>
-          <span className={`${styles.menuBtn} material-icons`}>menu</span>
-        </nav>
+    <>
+      <div className={styles.mobileMenu} ref={mobileMenuRef}>
+        <div className={styles.menuOptionsContainer}>
+          <a href="#about" className={styles.mobileMenuOption}>About</a>
+          <a href="#services" className={styles.mobileMenuOption}>Services</a>
+          <a href="#skills" className={styles.mobileMenuOption}>Skills</a>
+          <a href="#projects" className={styles.mobileMenuOption}>Projects</a>
+          <a href="#experience" className={styles.mobileMenuOption}>Experience</a>
+          <a href="#contact" className={styles.mobileMenuOption}>Contact</a>
+        </div>
       </div>
-    </header>
 
+      <header className={styles.headerSection}>
+        <div className={styles.headerContainer}>
+          <span className={`${styles.logo} material-icons`}>home</span>
+          <nav className={styles.navLinksContainer}>
+            <a href="#about" className={styles.navLink}>About</a>
+            <a href="#services" className={styles.navLink}>Services</a>
+            <a href="#skills" className={styles.navLink}>Skills</a>
+            <a href="#projects" className={styles.navLink}>Projects</a>
+            <a href="#experience" className={styles.navLink}>Experience</a>
+            <a href="#contact" className={styles.navLink}>Contact</a>
+            <span className={`${styles.toggleThemeBtn} material-icons`} onClick={()=>changeTheme()} ref={themeButtonRef}>dark_mode</span>
+            <span className={`${styles.menuBtn} material-icons`} onClick={()=>openMenu()} ref={mobileMenuBtnRef} >menu</span>
+          </nav>
+
+        </div>
+      </header>
+    </>
+
+   
   );
 }
 
