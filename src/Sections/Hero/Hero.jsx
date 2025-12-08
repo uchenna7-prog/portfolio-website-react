3bc934dbf548d670a4bd89fb2a434147118d47a4
uchenna7-prog <uchenduuchenna7@gmail.com>
2025-12-08 14:32:04 +0100

    made hero section responsive
    made hero section responsive

--- src/Sections/Hero/Hero.jsx
+++ src/Sections/Hero/Hero.jsx
@@ -4,21 +4,26 @@ import {Link} from "react-router-dom"
 function Hero(){
     return(
         <section className={styles.heroSection} id="home">
+            
             <div>
                 <h1 className={styles.name}>UCHENDU UCHENNA</h1>
                 <div className={styles.briefIntro}>Front-end Web Developer | Computer Science Student.</div>
 
                 <div className={styles.buttonsContainer}>
-                    <button className={`${styles.button} ${styles.hireMeBtn}`}>Hire Me</button>
+                    <button className={`${styles.button} ${styles.hireMeBtn}`}>
+                        <i class="fa-solid fa-briefcase"></i>
+                        Hire Me
+                    </button>
                     <Link to="/Resume" className="link">
-                        <button className={`${styles.button} ${styles.resumeBtn}`}>Résumé</button>
+                        <button className={`${styles.button} ${styles.resumeBtn}`}>
+                            <i className="fa-regular fa-file-lines"></i>
+                            Résumé
+                        </button>
                     </Link>
-                    
 
                 </div>
 
             </div>
-
 
         </section>
     )
